37abc20af534d32771c92405486a1fc16b6ba411
XD <clement.muzard@etu.umontpellier.fr>
2025-11-27 00:59:44 +0100

    feat: test benchmark on 1M

--- main.c
+++ main.c
@@ -1,20 +1,31 @@
+#include <bits/time.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 #include "ressourceManager.h"
+
+
+
+static inline uint64_t ns() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (uint64_t)ts.tv_sec * 1000000000ULL + ts.tv_nsec;
+}
 
 int main() {
 
-    RessourceManager *t = create_rm(10);
+    RessourceManager *rm = create_rm(1000000);
 
-    
-    for(int i =0; i<10; i++) {
-        printf("%p\n", (void*)t->handles[i]);
-        printf("%u\n", t->handles[i]->val);
-    }
-    
+    uint64_t start = ns();
+    uint64_t s = traverse_next(rm);
+    uint64_t end = ns();
+
+    printf("Resultat: %lu\n", s);
+    printf("Temps: %lu\n", end-start);
 
     // Free
-    free_rm(t);
+    free_rm(rm);
 
     return 0;
 }
