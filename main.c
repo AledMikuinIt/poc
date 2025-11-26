0f46940f92642de06047eabf88366641298a0fba
XD <clement.muzard@etu.umontpellier.fr>
2025-11-27 00:33:39 +0100

    refactor: include ressourceManager header

--- main.c
+++ main.c
@@ -1,46 +1,6 @@
 #include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Object {
-    struct Object *next;    // pointeur principal
-    struct Object *alt;     // pointeur alternatif
-    struct Object *meta;    // pointeur additionel
-    int val;     // valeur
-
-} Object;
-
-typedef struct RessourceManager {
-    Object **handles; // tableau de pointeurs
-    size_t num_objects; 
-} RessourceManager;
-
-
-RessourceManager* create_rm(size_t n) {
-    RessourceManager* rm = malloc(sizeof(RessourceManager));    // on crée un tableau de la taille de la struct
-    rm->num_objects = n;                                              // le nombre d'objet souhaité
-    rm->handles = malloc(n * sizeof(Object*));                  // les handles ont la taille du nombre de pointeurs de taille des pointeurs Objet
-
-    for(int i = 0; i < n; i++) {
-        rm->handles[i] = malloc(sizeof(Object));                // handle prends taille struct objet
-        rm->handles[i]->val = i;                                        // val = i comme ca c'est un peu comme l'id
-        rm->handles[i]->next = (i < n-1) ? rm->handles[i+1] : NULL;     // le handle pointe vers le suivant, mais a la fin il pointe vers NULL
-        rm->handles[i]->alt = NULL;                                     // pointe vers NULL
-        rm->handles[i]->meta = NULL;                                    // pointe vers NULL
-    }
-    return rm;
-}
-
-void free_rm(RessourceManager *rm) {
-
-    // Libération de mémoire 
-    for(int i = 0; i < rm->num_objects; i++) {
-        free(rm->handles[i]);
-    }
-    free(rm->handles);
-    free(rm);
-
-}
+#include "ressourceManager.h"
 
 int main() {
 
