9ba33f076735629c03877167418c33623112cbe8
XD <clement.muzard@etu.umontpellier.fr>
2025-11-27 03:18:01 +0100

    feat: double test benchmark for different RessourceManager load ressources

--- main.c
+++ main.c
@@ -15,17 +15,32 @@ static inline uint64_t ns() {
 
 int main() {
 
-    RessourceManager *rm = create_rm(1000000);
 
-    uint64_t start = ns();
-    uint64_t s = traverse_next(rm);
-    uint64_t end = ns();
+    size_t num = 1000000;
 
-    printf("Resultat: %lu\n", s);
-    printf("Temps: %lu\n", end-start);
+    RessourceManager *drm = create_dispersed_rm(num);
+    RessourceManager *lrm = create_linear_rm(num);
+
+    uint64_t dstart = ns();
+    uint64_t ds = traverse_next(drm);
+    uint64_t dend = ns();
+    uint64_t lstart = ns();
+    uint64_t ls = traverse_next(lrm);
+    uint64_t lend = ns();
+
+    printf("Resultat Lineaire: %lu\n",ls);
+    printf("Temps Lineaire: %lu\n\n", lend-lstart);
+
+
+    printf("Resultat Disperse: %lu\n", ds);
+    printf("Temps Disperse: %lu\n\n", dend-dstart);
+
+    printf("Différence: %lu\n", (dend-dstart)-(lend-lstart));
+
 
     // Free
-    free_rm(rm);
+    free_rm(drm);
+    free_rm(lrm);
 
     return 0;
 }
